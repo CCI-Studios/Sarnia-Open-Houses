b4f9e5f8ee888925ba08154bf9078223eee5aed4
Jonathan Bennett <jbennett@ccistudios.com>
2011-09-28 10:18:40 -0400

    If no id is given, redirect to self or home

--- com_openhouse/components/com_openhouse/controllers/agent.php
+++ com_openhouse/components/com_openhouse/controllers/agent.php
@@ -7,18 +7,32 @@ class ComOpenhouseControllerAgent extends ComDefaultControllerDefault
 	{
 		parent::__construct($config);
 		
-		$this->registerCallback('before.read', array($this, 'checkValid'));
-		
-		$this->_view = "agent";	// There is no agents listings
+		$this->registerCallback('before.read', array($this, 'redirectProfile'));
 	}
 	
-	public function checkValid(KCommandContext $context)
+	/**
+	 * There is not option to view and 'agents' style listing page.
+	 */
+	public function getView()
 	{
-		$data = $context->data;
+		$this->_view = "agent";
+		return parent::getView();
+	}
+	
+	public function redirectProfile(KCommandContext $context)
+	{
+		if (is_numeric(KRequest::get('get.id', 'int'))) {
+			return;
+		}
 		
+		$app = JFactory::getApplication();
+		$self = KFactory::get('site::com.openhouse.model.agents')->getMe();
 		
-		
-		KFactory::get('lib.joomla.application')->enqueueMessage('adf', 'infoasdf');
+		if ($self) {
+			$app->redirect('index.php?option=com_openhouse&view=agent&id='. $self->id);
+		} else {
+			$app->redirect('index.php', 'Agent not found.');
+		}
 		
 	}
 	
