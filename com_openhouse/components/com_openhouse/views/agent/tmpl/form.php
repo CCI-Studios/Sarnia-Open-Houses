f92ef6423ad9438ff611516aebfd6906f418f3d0
Jonathan Bennett <jbennett@ccistudios.com>
2011-09-28 10:20:28 -0400

    Added agent edit page

---
+++ com_openhouse/components/com_openhouse/views/agent/tmpl/form.php
@@ -0,0 +1,43 @@
+<?= @helper('behavior.mootools'); ?>
+<script src="media://lib_koowa/js/koowa.js" />
+<style src="media://com_openhouse/css/openhouse.css" />
+
+<h2>
+	Editing <?= $agent->name ?>'s Profile
+</h2>
+
+<form action="<?= @route('view=agent&id='. $agent->id) ?>" method="post" class="-koowa-form">
+	
+	<div class="field">
+		<label for="field_name"><?= @text('Name') ?>:</label><br>
+		<input type="text" id="field_name" name="name" value="<?= $agent->name ?>" />
+	</div>
+	
+	<div class="field">
+		<label for="field_title"><?= @text('Title') ?>:</label><br>
+		<input type="text" id="field_title" name="title" value="<?= $agent->title ?>" />
+	</div>
+	
+	<div class="field">
+		<label for="field_phone"><?= @text('Phone Number') ?>:</label><br>
+		<input type="text" id="field_phone" name="phone" value="<?= $agent->phone ?>" />
+	</div>
+	
+	<div class="field">
+		<label for="field_website"><?= @text('Website Address') ?>:</label><br>
+		<input type="text" id="field_website" name="website" value="<?= $agent->website ?>" />
+	</div>
+	
+	<div class="field">
+		<label for="field_email"><?= @text('Email Address') ?>:</label><br>
+		<input type="text" id="field_email" name="email" value="<?= $agent->email ?>" />
+	</div>
+	
+	office<br/>
+	company<br/>
+	
+	<div class="actions">
+		<button>Submit</button>
+		<a href="<?= @route('view=agent&id='. $agent->id) ?>" class="button">Done</a>
+	</div>
+</form>
