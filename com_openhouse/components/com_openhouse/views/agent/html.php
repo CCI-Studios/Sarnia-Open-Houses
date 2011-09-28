439f0700e7351456ad79edf0d4802591216ec5b4
Jonathan Bennett <jbennett@ccistudios.com>
2011-09-28 10:18:03 -0400

    Added initial layout of agent page

--- com_openhouse/components/com_openhouse/views/agent/html.php
+++ com_openhouse/components/com_openhouse/views/agent/html.php
@@ -12,10 +12,25 @@ class ComOpenhouseViewAgentHtml extends ComDefaultViewHtml
 				break;
 			case 'default':
 			default:
-				$this->assign('valid', $this->getModel()->isValid() === true);
+				$this->viewAgent();
 		}
 		
 		return parent::display();
 	}
 	
+	
+	private function viewAgent() {
+		$app = JFactory::getApplication();
+		$agent = $this->getModel()->getItem();
+		
+		$agent->isOwnable();
+		
+		if ($this->getModel()->isValid() !== true && $agent->canEdit()) {
+			$url = JRoute::_('index.php?option=com_openhouse&view=agent&layout=form&id='. $agent->id);
+			$app->enqueueMessage('Your profile is incomplete. Please <a href="'. $url .'">update your profile</a>.', 'notice');
+		}
+		
+		$listings = "ADSf"; // TODO update this
+		$this->assign('listings', $listings);
+	}
 }
--- com_openhouse/components/com_openhouse/views/agent/tmpl/default.php
+++ com_openhouse/components/com_openhouse/views/agent/tmpl/default.php
@@ -1,4 +1,37 @@
+<style src="media://com_openhouse/css/openhouse.css" />
+
 <h2>
 	<?= $agent->name ?>
-	<small> - <?= $agent->title ?>
+	<small>
+		 - <?= $agent->title ?>
+		<? if ($agent->canEdit()): ?>
+			- <a href="<?= @route('view=agent&layout=form&id='. $agent->id) ?>">Update profile</a>
+		<? endif; ?>
+	</small>
 </h2>
+
+
+<img class="profile-picture fltlft" src="http://dummyimage.com/150x200/ddd/333.jpg&amp;text=Profile" />
+
+<dl class="fltlft data-set">
+	<? if ($agent->phone): ?>
+		<dt><?= @text('Phone Number') ?>:</dt>
+		<dd><?= $agent->phone ?></dd>
+	<? endif; ?>
+
+	<? if ($agent->website): ?>
+		<dt><?= @text('Website') ?>:</dt>
+		<dd><a href="<?= $agent->website ?>" target="_blank"><?= $agent->website ?></a></dd>
+	<? endif; ?>
+
+	<? if ($agent->email): ?>
+		<dt><?= @text('Email') ?>:</dt>
+		<dd><a href="mailto:<?= $agent->email ?>"><?= $agent->email ?></a></dd>
+	<? endif; ?>
+</dl>
+
+<div class="clear"></div>
+
+
+<?= $listings ?>
+<p><a href="<?= @route('view=listing&layout=form') ?>">Add new listing</a></p>
