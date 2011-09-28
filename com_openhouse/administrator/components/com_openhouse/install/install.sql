eaa4051e076e359fb04f50923b63ad920e414f4e
Jonathan Bennett <jbennett@ccistudios.com>
2011-09-28 10:19:16 -0400

    Added other needed tables

--- com_openhouse/administrator/components/com_openhouse/install/install.sql
+++ com_openhouse/administrator/components/com_openhouse/install/install.sql
@@ -32,9 +32,37 @@ CREATE TABLE IF NOT EXISTS `#__openhouse_offices` (
 	`title` VARCHAR(250) NOT NULL
 ) ENGINE=MyISAM;
 
-# company
-	# name
-	# number
-	# logo
-# office
+CREATE TABLE IF NOT EXISTS `#__openhouse_listings` (
+	`openhouse_listing_id` SERIAL,
+	
+	`price` VARCHAR(15) NOT NULL,
+	`bedrooms` VARCHAR(10) NOT NULL,
+	`bathrooms` VARCHAR(10) NOT NULL,
+	`address` VARCHAR(30) NOT NULL,
+	`city` VARCHAR(20) NOT NULL,
+	`postal` VARCHAR(20) NOT NULL,
+	`description` VARCHAR(500) NOT NULL,
+	`enabled` TINYINT(1) NOT NULL DEFAULT '0'
+	
+) ENGINE=MyISAM;
 
+CREATE TABLE IF NOT EXISTS `#__openhouse_images` (
+	`openhouse_image_id` SERIAL,
+	
+	`filename` VARCHAR(250) NOT NULL,
+	`title` VARCHAR(200) NOT NULL,
+	`enabled` TINYINT(1) NOT NULL DEFAULT '0',
+	
+	`ordering` TINYINT(3) NOT NULL DEFAULT '0',
+	`openhouse_listing_id` BIGINT(20) UNSIGNED NOT NULL
+) ENGINE=MyISAM;
+
+CREATE TABLE IF NOT EXISTS `#__openhouse_showings` (
+	`openhouse_showing_id` SERIAL,
+	
+	`start_date` DATE NOT NULL,
+	`end_date` DATE NOT NULL,
+	`hours` VARCHAR(50) NOT NULL,
+	
+	`openhouse_listing_id` BIGINT(20) UNSIGNED NOT NULL
+) ENGINE=MyISAM;
