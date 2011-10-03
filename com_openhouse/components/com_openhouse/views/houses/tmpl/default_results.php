03be60d74654f0e643203c394dfbceea4f3825ba
Jonathan Bennett <jbennett@ccistudios.com>
2011-10-03 16:59:51 -0400

    Listings Style

--- com_openhouse/components/com_openhouse/views/houses/tmpl/default_results.php
+++ com_openhouse/components/com_openhouse/views/houses/tmpl/default_results.php
@@ -1,5 +1,17 @@
-<ul>
-	<? for ($i = 0; $i < 20; $i++): ?>
-		<li>adsf</li>
-	<? endfor; ?>
-</ul>
+<style src="media://com_openhouse/css/openhouse.css" />
+
+<ul class="openhouse-listings"><? for ($i = 0; $i < 3; $i++): ?>
+	<li>
+		<div class="image">
+			<img src="images/default/home.png" />
+			<div class="price"><span>$159,900</span></div>
+		</div>
+		<div class="description">
+			<h2>110 Water Street<br />
+				<small>Sarnia, Ontario</small></h2>
+			<p>Donec vel ligula vel augue eleifend elementum. Donec eget orci ac nulla congue tincidunt non eu ligula. Ut pellentesque dapibus...</p>
+			<a href="" class="button"><span>View Listing</span></a>
+		</div>
+		<div class="clear"></div>
+	</li>
+<? endfor; ?></ul>
