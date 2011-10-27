5dcdddc55b52663f053f3dc02755ecf925cd6664
Jonathan Bennett <jbennett@ccistudios.com>
2011-10-27 11:34:52 -0400

    Moved Gallery CSS into its own file

--- com_openhouse/components/com_openhouse/views/images/tmpl/gallery.php
+++ com_openhouse/components/com_openhouse/views/images/tmpl/gallery.php
@@ -1,15 +1,16 @@
 <? defined('KOOWA') or die ?>
 <?= @helper('behavior.mootools') ?>
 <script src="media://com_openhouse/js/gallery.js" />
+<style src="media://com_openhouse/css/gallery.css" />
 
 <? if (count($images)): ?>
-	<div class="openhouse-gallery">
-		<div class="main-image"></div>
-	
-		<ul>
-			<? foreach($images as $image): ?>
-				<li><img src="media://com_openhouse/uploads/<?= $image->filename ?>" height="65" /></li>
-			<? endforeach; ?>
-		</ul>
-	</div>
+<div class="openhouse-gallery">
+	<div class="main-image"></div>
+
+	<ul>
+		<? foreach($images as $image): ?>
+			<li><img src="media://com_openhouse/uploads/small/<?= $image->filename ?>" height="27" /></li>
+		<? endforeach; ?>
+	</ul>
+</div>
 <? endif; ?>
