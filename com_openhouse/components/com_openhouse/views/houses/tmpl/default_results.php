114af62fe44e82018e5857874d44eb82100a4828
Jonathan Bennett <jbennett@ccistudios.com>
2011-11-19 23:10:38 -0500

    Added bottom shadow to house image

--- com_openhouse/components/com_openhouse/views/houses/tmpl/default_results.php
+++ com_openhouse/components/com_openhouse/views/houses/tmpl/default_results.php
@@ -2,13 +2,13 @@
 
 <ul class="openhouse-listings"><? foreach ($houses as $house): ?>
 	<li>
-		<div class="image">
+		<div class="image"><div>
 			<img src="images/default/home.png" />
 			
 			<div class="price"><span>
 				$<?= $house->price ?>
 			</span></div>
-		</div>
+		</div></div>
 		
 		<div class="description">
 			<h2><?= $house->address ?><br />
