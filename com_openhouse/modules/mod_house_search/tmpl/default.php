cb951e0c188af91fc4a6088c54d6bfef89d60c80
Jonathan Bennett <jbennett@ccistudios.com>
2011-11-21 00:04:49 -0500

    Changed formatting for form

--- com_openhouse/modules/mod_house_search/tmpl/default.php
+++ com_openhouse/modules/mod_house_search/tmpl/default.php
@@ -1,21 +1,24 @@
 <form action="" method="get" class="openhouse-quick-search -koowa-grid">
 	
-	<div class="field">
-		<label>Min Price</label>
-		<input type="text" name="min-price" />
-	</div>
-	
-	<div class="field">
-		<label>Max Price</label>
-		<input type="text" name="max-price" />
-	</div>
-	
-	<div class="field">
-		<label>Location</label>
-		<?= @helper('com://site/openhouse.template.helper.listbox.locations') ?>
-	</div>
-	
-	<div class="actions center">
-		<button class="red"><span>Search</span></button>
-	</div>
+	<table>
+		<tr>
+			<td align="right"><label><?= @text('Min Price') ?>:</label></td>
+			<td><input type="text" name="min-price" /></td>
+		</tr>
+		
+		<tr>
+			<td align="right"><label><?= @text('Max Price') ?>:</label></td>
+			<td><input type="text" name="max-price" /></td>
+		</tr>
+		
+		<tr>
+			<td align="right"><label><?= @text('Location') ?>:</label></td>
+			<td><?= @helper('com://site/openhouse.template.helper.listbox.locations') ?></td>
+		</tr>
+		
+		<tr>
+			<td>&nbsp;</td>
+			<td><button class="red"><span>Search</span></button></td>
+		</tr>
+	</table>
 </form>
