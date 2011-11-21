6599c1e15c30266fcd8e012da878f194b5dec3c6
Jonathan Bennett <jbennett@ccistudios.com>
2011-11-21 09:14:13 -0500

    Allow agents to update their profile

--- com_openhouse/components/com_openhouse/controllers/behaviors/executable.php
+++ com_openhouse/components/com_openhouse/controllers/behaviors/executable.php
@@ -3,4 +3,16 @@
 class ComOpenHouseControllerBehaviorExecutable extends ComDefaultControllerBehaviorExecutable
 {
 
+	public function canEdit()
+	{
+		$name = $this->getMixer()->getIdentifier()->name;
+		$user = JFactory::getUser();
+		
+		if ($name === 'agent') {
+			$item = $this->getMixer()->getModel()->getItem();
+			return ($user->id == $item->user_id);
+		}
+		
+		return parent::canEdit();
+	}
 }
