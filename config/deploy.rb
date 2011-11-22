0293a919735bf76ce3f585d4949099608c85370a
Jonathan Bennett <jbennett@ccistudios.com>
2011-11-22 14:49:27 -0500

    Updated deploy configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,14 +3,14 @@ set :stages, %w(staging production)
 set :default_stage, "staging"
 require "capistrano/ext/multistage"
 
-set :application, "APPLICATION_NAME"
+set :application, "Sarnia Lambton Open Houses"
 
 # repository info
-set :repository,  "git@github.com:CCI-Studios/PROJECT.git"
+set :repository,  "git@github.com:CCI-Studios/Sarnia-Open-Houses.git"
 set :scm, :git
 
 # ssh settings
-set :user, "USERNAME"
+set :user, "sloh"
 set :use_sudo, false
 
 # Joomla
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,10 +1,10 @@
 # repository info
-set :branch, "development"
+set :branch, "master"
 
 # This may be the same as your `Web` server
 role :app, "sarnialambtonopenhouses.ca"
 
 # directories
-set :deploy_to, "/home/sloh/subdomains/dev"
+set :deploy_to, "/home/sloh/subdomains/live"
 set :public, "#{deploy_to}/public_html"
 set :extensions, %w[com_openhouse filogix_auth public template]
