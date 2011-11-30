1d1cb956019cd810642a70967fee3245192c3703
Jonathan Bennett <jbennett@ccistudios.com>
2011-11-30 12:42:14 -0500

    Added map poser image

--- com_openhouse/components/com_openhouse/views/house/tmpl/default_map.php
+++ com_openhouse/components/com_openhouse/views/house/tmpl/default_map.php
@@ -1,9 +1,12 @@
+<? @helper('behavior.mootools') ?>
+<? @helper('behavior.modal') ?>
+
 <?php
-	$address = urlencode('110 Water St, Sarnia, On N7T 5T2, Canada');
+	$address = $house->address .','. $house->city .','. $house->province .','. $house->postal;
 	$width = 267;
 	$height = 153;
-	$url = 'http://maps.google.com/maps?';
-	
+
+	$dynamic_url = 'http://maps.google.com/maps?';
 	$map = array();
 	$map['f'] = 'q';
 	$map['hl'] = 'en';
@@ -11,16 +14,19 @@
 	$map['ie'] = 'UTF8';
 	$map['hnear'] = $address;
 	$map['t'] = 'm';
-	$map['z'] = '14';
+	$map['z'] = 14;
 	$map['iwloc'] = '';
 	$map['output'] = 'embed';
+
+	$poser_url = "http://maps.googleapis.com/maps/api/staticmap?";
+	$poser = array();
+	$poser['center'] = $address;
+	$poser['zoom'] = 14;
+	$poser['size'] = $width .'x'. $height;
+	$poser['sensor'] = 'false';
+	$poser['markers'] = 'color:0xff625f|'. $address;
 ?>
 
-<iframe
-	width="<?= $width ?>"
-	height="<?= $height ?>"
-	frameborder="0"
-	scrolling="no"
-	marginheight="0"
-	marginwidth="0"
-	src="<?= $url . http_build_query($map) ?>"></iframe>
+<a rel="{handler: 'iframe', size: {x: 540, y: 400}}" class="modal" href="<?= $dynamic_url . http_build_query($map) ?>">
+	<img src="<?= $poser_url . http_build_query($poser) ?>" width="<?= $width ?>" height="<?= $height ?>" style="display: block;" />
+</a>
