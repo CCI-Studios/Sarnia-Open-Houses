722ca70e319465bd6e8aa9496c2df414da96ef09
Jonathan Bennett <jbennett@ccistudios.com>
2011-11-30 21:52:10 -0500

    Made agent->user_id unique

--- com_openhouse/administrator/components/com_openhouse/install/install.sql
+++ com_openhouse/administrator/components/com_openhouse/install/install.sql
@@ -1,40 +1,40 @@
 CREATE TABLE IF NOT EXISTS `#__openhouse_agents` (
 	`openhouse_agent_id` SERIAL,
-	
+
 	`name` VARCHAR(250) NOT NULL,
 	`title` VARCHAR(250) NOT NULL,
 	`phone` VARCHAR(20) NOT NULL DEFAULT '',
 	`website` VARCHAR(250) NOT NULL DEFAULT '',
 	`email` VARCHAR(250) NOT NULL DEFAULT '',
-	
+
 	`openhouse_office_id` BIGINT(20) NOT NULL,
 	`openhouse_company_id` BIGINT(20) NOT NULL,
-	`user_id` INT(11) NOT NULL,
-	
+	`user_id` INT(11) NOT NULL UNIQUE,
+
 	UNIQUE KEY `user_id` (`user_id`)
 ) ENGINE=MyISAM;
 
 CREATE TABLE IF NOT EXISTS `#__openhouse_locations` (
 	`openhouse_location_id` SERIAL,
-	
+
 	`title` VARCHAR(250) NOT NULL
 ) ENGINE=MyISAM;
 
 CREATE TABLE IF NOT EXISTS `#__openhouse_companies` (
 	`openhouse_company_id` SERIAL,
-	
+
 	`title` VARCHAR(250) NOT NULL
 ) ENGINE=MyISAM;
 
 CREATE TABLE IF NOT EXISTS `#__openhouse_offices` (
 	`openhouse_office_id` SERIAL,
-	
+
 	`title` VARCHAR(250) NOT NULL
 ) ENGINE=MyISAM;
 
 CREATE TABLE IF NOT EXISTS `#__openhouse_houses` (
 	`openhouse_house_id` SERIAL,
-	
+
 	`price` VARCHAR(15) NOT NULL,
 	`bedrooms` VARCHAR(10) NOT NULL,
 	`bathrooms` VARCHAR(10) NOT NULL,
@@ -45,28 +45,28 @@ CREATE TABLE IF NOT EXISTS `#__openhouse_houses` (
 	`postal` VARCHAR(20) NOT NULL,
 	`description` VARCHAR(500) NOT NULL,
 	`enabled` TINYINT(1) NOT NULL DEFAULT '0',
-	
+
 	`created_on` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
 	`created_by` INT(11) NOT NULL DEFAULT 0
 ) ENGINE=MyISAM;
 
 CREATE TABLE IF NOT EXISTS `#__openhouse_images` (
 	`openhouse_image_id` SERIAL,
-	
+
 	`filename` VARCHAR(250) NOT NULL,
 	`title` VARCHAR(200) NOT NULL,
 	`enabled` TINYINT(1) NOT NULL DEFAULT '0',
-	
+
 	`ordering` TINYINT(3) NOT NULL DEFAULT '0',
 	`openhouse_house_id` BIGINT(20) UNSIGNED NOT NULL
 ) ENGINE=MyISAM;
 
 CREATE TABLE IF NOT EXISTS `#__openhouse_showings` (
 	`openhouse_showing_id` SERIAL,
-	
+
 	`start_date` DATE NOT NULL,
 	`end_date` DATE NOT NULL,
 	`hours` VARCHAR(50) NOT NULL,
-	
+
 	`openhouse_house_id` BIGINT(20) UNSIGNED NOT NULL
 ) ENGINE=MyISAM;
--- com_openhouse/administrator/components/com_openhouse/models/agents.php
+++ com_openhouse/administrator/components/com_openhouse/models/agents.php
@@ -2,72 +2,69 @@
 
 class ComOpenhouseModelAgents extends ComDefaultModelDefault
 {
-	
+
 	public function __construct(KConfig $config)
 	{
 		parent::__construct($config);
-		
-		$this->_state
-			->insert('user_id', 'int');
 	}
-	
+
 	protected function _buildQueryWhere(KDatabaseQuery $query)
 	{
 		$state = $this->_state;
-		
+
 		if (is_numeric($state->user_id)) {
 			$query->where('user_id', '=', $state->user_id);
 		}
-		
+
 		parent::_buildQueryWhere($query);
 	}
-	
+
 	public function getMe()
 	{
 		$user = JFactory::getUser();
-		
+
 		if ($user->guest) {
 			return null;
 		}
-		
+
 		$model = $this->getService($this->getIdentifier());
 		$list = $model->set('user_id', $user->id)->getList();
-		
+
 		if (!count($list)) {
 			return null;
 		}
-		
+
 		return $list->top();
-		
+
 	}
-		
+
 	public function isValid()
 	{
 		$item = $this->getItem();
-		
+
 		$errors = array();
-		
+
 		if (!$item->name) {
 			$errors[] = JText::_("Name cannot be blank");
 		}
-		
+
 		if (!$item->title) {
 			$errors[] = JText::_("Title cannot be blank.");
 		}
-		
+
 		if (!$item->phone) {
 			$errors[] = JText::_("You need to have a phone number listed.");
 		}
-		
+
 		if (!$item->openhouse_office_id) {
 			$errors[] = JText::_("You must specify your office.");
 		}
-		
+
 		if (!$item->openhouse_company_id) {
 			$errors[] = JText::_("You must select the your company.");
 		}
-	
+
 		return (count($errors))? $errors : true;
 	}
-	
+
 }
