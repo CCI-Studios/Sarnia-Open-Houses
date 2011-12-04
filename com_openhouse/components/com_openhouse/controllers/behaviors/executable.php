9378d2b7ebbcab15b84df9be064f16aa0dd7eccb
Jonathan Bennett <jbennett@ccistudios.com>
2011-12-03 19:41:47 -0500

    Improved authentication
    Needs work on:
    - images
    - profiles
    - fix showings bug
    
    Solves most of #2

--- com_openhouse/components/com_openhouse/controllers/behaviors/executable.php
+++ com_openhouse/components/com_openhouse/controllers/behaviors/executable.php
@@ -3,36 +3,49 @@
 class ComOpenHouseControllerBehaviorExecutable extends ComDefaultControllerBehaviorExecutable
 {
 
-	public function canEdit()
-	{
-		$name = $this->getMixer()->getIdentifier()->name;
-		$user = JFactory::getUser();
-		
-		if ($name === 'agent') {
-			$item = $this->getMixer()->getModel()->getItem();
-			return ($user->id == $item->user_id);
-		} elseif ($name === 'house') {
-			$item = $this->getMixer()->getModel()->getItem();
-			return ($user->id == $item->created_by);
-		}
-		
-		return parent::canEdit();
-	}
+	
 	
 	public function canAdd() {
 		$name = $this->getMixer()->getIdentifier()->name;
+		$item = $this->getMixer()->getModel()->getItem();
 		$user = JFactory::getUser();
-		$acl = JFactory::getAcl();
 		
-		if ($name === 'waypoint') {
-			return true;
-		} elseif ($name === 'house') {
-			$groups = $acl->getGroupsByUser($user->id);
-			if (in_array(9, $groups)) {
+		switch($name) {
+			case 'agent':
+				return false;
+			case 'house':
+				return parent::canAdd();
+			// image
+			// profile
+			case 'showing':
+				return parent::canAdd(); // FIXME any agent can create a listing for any house
+			case 'waypoint':
 				return true;
-			}
+			default:
+				return parent::canAdd();
 		}
+	}
+	
+	public function canEdit()
+	{
+		$name = $this->getMixer()->getIdentifier()->name;
+		$item = $this->getMixer()->getModel()->getItem();
+		$user = JFactory::getUser();
 		
-		return parent::canAdd();
+		
+		switch($name) {
+			case 'agent':
+				return ($user->id === $item->user_id) && parent::canEdit();
+			case 'house':
+				return ($user->id === $item->created_by) && parent::canEdit();
+			// image
+			// profile
+			case 'showing':
+				return false;
+			case 'waypoint':
+				return false;
+			default:
+				return parent::canEdit();
+		}
 	}
 }
