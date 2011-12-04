5c8a4562bfdada4f4ebea78b47bc8612bbf06643
Jonathan Bennett <jbennett@ccistudios.com>
2011-12-04 03:17:38 -0500

    Enabled quick search
    Closes #1

--- com_openhouse/modules/mod_house_search/tmpl/default.php
+++ com_openhouse/modules/mod_house_search/tmpl/default.php
@@ -1,24 +1,28 @@
-<form action="" method="get" class="openhouse-quick-search -koowa-grid">
+<form action="<?= @route('option=com_openhouse&view=houses') ?>" method="get" class="openhouse-quick-search -koowa-grid">
 
 	<table>
 		<tr>
 			<td align="right"><label><?= @text('Min Price') ?>:</label></td>
-			<td><input type="text" name="min-price" /></td>
+			<td><input type="text" name="min_price" value="<?= KRequest::get('get.min_price', 'raw') ?>" /></td>
 		</tr>
 
 		<tr>
 			<td align="right"><label><?= @text('Max Price') ?>:</label></td>
-			<td><input type="text" name="max-price" /></td>
+			<td><input type="text" name="max_price" value="<?= KRequest::get('get.max_price', 'raw') ?>" /></td>
 		</tr>
 
 		<tr>
 			<td align="right"><label><?= @text('Location') ?>:</label></td>
-			<td><?= @helper('com://admin/openhouse.template.helper.listbox.locations') ?></td>
+			<td><?= @helper('com://admin/openhouse.template.helper.listbox.locations', array(
+				'name' 	=> 'city',
+				'value'	=> 'title',
+				'selected' => KRequest::get('get.city', 'string')
+			)) ?></td>
 		</tr>
 
 		<tr>
 			<td>&nbsp;</td>
-			<td><div class="button red"><span>Search</span></div></td><!-- FIXME enable search -->
+			<td><button class="red"><span>Search</span></button></td>
 		</tr>
 	</table>
 </form>
