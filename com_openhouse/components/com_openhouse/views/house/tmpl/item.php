3040f9188eac96b682ebe60c0f72e605d2ad2e28
Jonathan Bennett <jbennett@ccistudios.com>
2011-12-04 23:54:14 -0500

    Use first house image for waypoints

--- com_openhouse/components/com_openhouse/views/house/tmpl/item.php
+++ com_openhouse/components/com_openhouse/views/house/tmpl/item.php
@@ -1,4 +1,16 @@
-<img src="images/default/home.png" width="75" class="left bordered" style="margin-right: 10px;" />
+<? if (count($house->images)): ?>
+	<img 
+		src="media://com_openhouse/uploads/large/<?= $house->images->current()->filename ?>"
+		width="75"
+		class="left bordered"
+		style="margin-right: 10px;" />
+<? else: ?>
+	<img
+		src="http://dummyimage.com/75x35/f80000/ffffff&text=Coming+Soon"
+		width="75"
+		class="left bordered"
+		style="margin-right: 10px;" />
+<?  endif; ?>
 
 <h2><a href="<?= @route('view=house&id='. $house->id) ?>">
 	<?= $house->address ?>
