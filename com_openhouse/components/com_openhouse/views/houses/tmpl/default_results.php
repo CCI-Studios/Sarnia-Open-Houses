d9e35aeb83f37444b35e59c351f7b471f1577c35
Jonathan Bennett <jbennett@ccistudios.com>
2011-12-05 14:03:08 -0500

    Add message if no houses match search request
    Fixes #33

--- com_openhouse/components/com_openhouse/views/houses/tmpl/default_results.php
+++ com_openhouse/components/com_openhouse/views/houses/tmpl/default_results.php
@@ -1,33 +1,41 @@
 <style src="media://com_openhouse/css/openhouse.css" />
 
-<ul class="openhouse-listings"><? foreach ($houses as $house): ?>
-	<li class="gradient">
-		<div class="image"><div>
-			<? if (count($house->images)): ?>
-				<img 
-					src="media://com_openhouse/uploads/large/<?= $house->images->current()->filename ?>"
-					width="275"
-					height="129" />
-			<? else: ?>
-				<img
-					src="media://com_openhouse/images/placeholder.jpg"
-					width="275"
-					height="129" />
-			<?  endif; ?>
+<ul class="openhouse-listings">
+	<? foreach ($houses as $house): ?>
+		<li class="gradient">
+			<div class="image"><div>
+				<? if (count($house->images)): ?>
+					<img 
+						src="media://com_openhouse/uploads/large/<?= $house->images->current()->filename ?>"
+						width="275"
+						height="129" />
+				<? else: ?>
+					<img
+						src="media://com_openhouse/images/placeholder.jpg"
+						width="275"
+						height="129" />
+				<?  endif; ?>
 			
-			<div class="price"><span><span>
-				<?= @format('price', $house->price) ?>
-			</span></span></div>
-		</div></div>
+				<div class="price"><span><span>
+					<?= @format('price', $house->price) ?>
+				</span></span></div>
+			</div></div>
 		
-		<div class="description">
-			<h2><?= $house->address ?><br />
-				<small><?= $house->city .', '. $house->province ?></small></h2>
-			<p><?= str_replace("\n\n", "</p><p>", substr($house->description, 0, 150)) ?>...</p>
-			<div class="center">
-				<a href="<?= @route('view=house&id='. $house->id) ?>" class="button"><span>View Listing</span></a>
+			<div class="description">
+				<h2><?= $house->address ?><br />
+					<small><?= $house->city .', '. $house->province ?></small></h2>
+				<p><?= str_replace("\n\n", "</p><p>", substr($house->description, 0, 150)) ?>...</p>
+				<div class="center">
+					<a href="<?= @route('view=house&id='. $house->id) ?>" class="button"><span>View Listing</span></a>
+				</div>
 			</div>
-		</div>
-		<div class="clear"></div>
+			<div class="clear"></div>
+		</li>
+	<? endforeach; ?>
+	
+	<? if (!count($houses)): ?>
+	<li class="gradient">
+		<p>There are no houses available that match your request.</p>
 	</li>
-<? endforeach; ?></ul>
+	<? endif; ?>
+</ul>
