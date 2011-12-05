af8c16cc564cabd4232f2e784219fc02587bb52a
Jonathan Bennett <jbennett@ccistudios.com>
2011-12-05 10:54:17 -0500

    Updating forms

--- com_openhouse/modules/mod_house_search/tmpl/default.php
+++ com_openhouse/modules/mod_house_search/tmpl/default.php
@@ -3,17 +3,17 @@
 	<table>
 		<tr>
 			<td align="right"><label for="mod_min_price"><?= @text('Min Price') ?>:</label></td>
-			<td><input type="text" id="mod_min_price" name="min_price" value="<?= KRequest::get('get.min_price', 'raw') ?>" /></td>
+			<td width="130"><input type="text" id="mod_min_price" name="min_price" value="<?= KRequest::get('get.min_price', 'raw') ?>" /></td>
 		</tr>
 
 		<tr>
 			<td align="right"><label for="mod_max_price" ><?= @text('Max Price') ?>:</label></td>
-			<td><input type="text" name="max_price" id="mod_max_price" value="<?= KRequest::get('get.max_price', 'raw') ?>" /></td>
+			<td width="130"><input type="text" name="max_price" id="mod_max_price" value="<?= KRequest::get('get.max_price', 'raw') ?>" /></td>
 		</tr>
 
 		<tr>
 			<td align="right"><label><?= @text('Location') ?>:</label></td>
-			<td><?= @helper('com://admin/openhouse.template.helper.listbox.locations', array(
+			<td width="130"><?= @helper('com://admin/openhouse.template.helper.listbox.locations', array(
 				'name' 	=> 'city',
 				'value'	=> 'title',
 				'selected' => KRequest::get('get.city', 'string')
