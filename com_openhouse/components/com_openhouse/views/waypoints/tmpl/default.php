762e55008c8bd9df3d0066ed5c4e746d2e2c7bf8
Jonathan Bennett <jbennett@ccistudios.com>
2011-12-05 12:00:02 -0500

    Updated cart layout

--- com_openhouse/components/com_openhouse/views/waypoints/tmpl/default.php
+++ com_openhouse/components/com_openhouse/views/waypoints/tmpl/default.php
@@ -1,22 +1,40 @@
 <style src="media://com_openhouse/css/openhouse.css" />
-<script src="media://com_openhouse/js/dotter.js" />
-<script src="media://com_openhouse/js/handlers.js" />
+<script src="media://lib_koowa/js/koowa.js" />
 
 <module title="Directions" position="sidebar">
 	<?= @template('default_map') ?>
 	<p class="center">Click the map for directions</p>
 </module>
 
-<div>
-	<? foreach($waypoints as $wp): ?>
-	<div class="gradient">
-		<?= @template('com://site/openhouse.view.house.item', array('house'=>$wp->house)); ?>
-		
-		<div class="button remove_from_cart" data-id="<?= $wp->id ?>" data-token="<?= JUtility::getToken() ?>">
-			<span>Remove from Cart</span>
-		</div>
-	</div>
-	<? endforeach; ?>
-
-	<div class="clear"></div>
-</div>
+<form action="<?= @route('view=waypoints') ?>" method="get" class="-koowa-grid">
+	<table>
+		<? foreach($waypoints as $wp): ?>
+		<tr>
+			<td width="100" align="center" valign="top">
+				<? if ($wp->house->images->current()): ?>
+					<img 
+						src="media://com_openhouse/uploads/large/<?= $wp->house->images->current()->filename ?>"
+						width="75"
+						class="bordered" />
+				<? else: ?>
+					<img 
+						src="media://com_openhouse/images/placeholder.jpg"
+						width="75"
+						class="bordered" />
+				<? endif; ?>
+			</td>
+			<td valign="top">
+				<h2><a href="<?= @route('view=house&id='. $wp->house->id) ?>"><?= $wp->house->address ?></a></h2>
+				<p><?= @format('price', $wp->house->price) ?></p>
+			</td>
+			<td width="175" valign="top">
+				<div class="hidden"><?= @helper('grid.checkbox', array('row' => $wp)) ?></div>
+				
+			 	<div class="button" data-action="delete" data>
+					<span>Remove from Cart</span>
+				</div>
+			</td>
+		</tr>
+		<? endforeach; ?>
+	</table>
+</form>
