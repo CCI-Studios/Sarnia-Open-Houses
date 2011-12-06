1746d63bdd9db53cf04d275fb09aa1500bd85fea
Jonathan Bennett <jbennett@ccistudios.com>
2011-12-06 10:13:41 -0500

    Added Favicon
    Watching to see if this helps #38

--- template/templates/soh/index.php
+++ template/templates/soh/index.php
@@ -35,7 +35,7 @@ JHtml::_('behavior.mootools');
  	<jdoc:include type="head" />
 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="shortcut icon" href="/templates/<?= $this->template ?>/resources/favicon.ico">
+	<link rel="shortcut icon" type="image/x-icon" href="/templates/<?= $this->template ?>/resources/favicon.ico">
 	<link rel="apple-touch-icon" href="/templates/<?= $this->template ?>/resources/apple-touch-icon.png">
 
 	<!-- load css -->
