3b57d522b38e752067c8842a0233dd25eb9e51f2
Jonathan Bennett <jbennett@ccistudios.com>
2012-01-20 13:56:20 -0500

    Added Google Analytics

--- template/templates/soh/index.php
+++ template/templates/soh/index.php
@@ -24,7 +24,7 @@ if ($_SERVER['SERVER_PORT'] === 8888 ||
 	$testing = false;
 }
 
-$analytics = "UA-XXXXX-X"; // FIXME Update to client ID
+$analytics = "UA-28509530-1"; // FIXME Update to client ID
 JHtml::_('behavior.mootools');
 ?>
 
