e306dfd957f51906cae4ce2340e7f6e760c8cbfa
Jonathan Bennett <jbennett@ccistudios.com>
2012-01-23 12:16:17 -0500

    Updated open house list page

--- com_openhouse/administrator/components/com_openhouse/views/houses/tmpl/default.php
+++ com_openhouse/administrator/components/com_openhouse/views/houses/tmpl/default.php
@@ -1,9 +1,42 @@
-<? defined('KOOWA') or die ?>
+<? defined('KOOWA') or die; ?>
+<? @helper('behavior.mootools') ?>
+<script src="media://lib_koowa/js/koowa.js" />
+<style src="media://lib_koowa/css/koowa.css" />
 
-<ul>
-	<? foreach($houses as $house): ?>
-	<li><a href="<?= @route('view=house&id='. $house->id) ?>">
-		<?= "{$house->address}: {$house->id}" ?>
-	</a></li>
-	<? endforeach; ?>
-</ul>
+<form action="<?= @route() ?>" method="get" class="-koowa-grid">
+	<table class="adminlist">
+		<thead>
+			<tr>
+				<th width="1%"><?= @helper('grid.checkall') ?></th>
+				<th><?= @helper('grid.sort', array('title' => 'address')) ?></th>
+				<th width="100"><?= @helper('grid.sort', array('title' => 'city')) ?></th>
+				<th width="100"><?= @helper('grid.sort', array('title' => 'province')) ?></th>
+				<th width="100"><?= @helper('grid.sort', array('title' => 'postal')) ?></th>
+				<th width="100"><?= @helper('grid.sort', array('title' => 'price')) ?></th>
+				<th width="10"><?= @helper('grid.sort', array('title' => 'created_by')) ?></th>
+			</tr>
+		</thead>
+
+		<tfoot>
+			<tr>
+				<td colspan="7" align="center"><?= @helper('paginator.pagination', array('total'=>$total)) ?></td>
+			</tr>
+		</tfoot>
+
+		<tbody>
+			<? foreach ($houses as $house): ?>
+			<tr>
+				<td align="center"><?= @helper('grid.checkbox', array('row'=>$house)) ?></td>
+				<td><a href="<?= @route('view=house&id='. $house->id) ?>">
+					<?= $house->address ?>
+				</a></td>
+				<td align="center"><?= $house->city ?></td>
+				<td align="center"><?= $house->province ?></td>
+				<td align="center"><?= $house->postal ?></td>
+				<td align="center"><?= $house->price ?></td>
+				<td align="center"><?= $house->created_by ?></td>
+			</tr>
+			<? endforeach; ?>
+		</tbody>
+	</table>
+</form>
