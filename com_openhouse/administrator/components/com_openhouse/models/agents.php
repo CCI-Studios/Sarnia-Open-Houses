e9af9525b7999c685c790f2860dbf426ccc7897a
Jonathan Bennett <jbennett@ccistudios.com>
2012-01-23 12:49:42 -0500

    Added default sort order

--- com_openhouse/administrator/components/com_openhouse/models/agents.php
+++ com_openhouse/administrator/components/com_openhouse/models/agents.php
@@ -6,6 +6,9 @@ class ComOpenhouseModelAgents extends ComDefaultModelDefault
 	public function __construct(KConfig $config)
 	{
 		parent::__construct($config);
+		
+		$this->_state
+			->remove('sort')->insert('sort', 'cmd', 'name');
 	}
 
 	protected function _buildQueryWhere(KDatabaseQuery $query)
