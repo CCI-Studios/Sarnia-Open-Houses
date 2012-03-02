4833f2dae1a052b1a1c00a3035b286d5fded15a5
Jonathan Bennett <jbennett@ccistudios.com>
2012-03-02 13:51:08 -0500

    Increase time limit

--- com_openhouse/administrator/components/com_openhouse/controllers/image.php
+++ com_openhouse/administrator/components/com_openhouse/controllers/image.php
@@ -5,6 +5,8 @@ class ComOpenhouseControllerImage extends ComDefaultControllerDefault
 
 	protected function _actionResize(KCommandContext $context)
 	{
+		set_time_limit(500);
+		
 		$model = $this->getService('com://admin/openhouse.model.images');
 		$images = $model->getList();
 
